60266d5f8c276e25c9bcc4f6c9f9022fe9d06a0a
Leoni Mella <leoni.mella@gmail.com>
2020-10-29 21:33:22 -0300

    Added Tesla client

--- lib/parallel_download.ex
+++ lib/parallel_download.ex
@@ -6,6 +6,21 @@ defmodule ParallelDownload do
   alias ParallelDownload.Client
 
   def start(urls) do
-    Enum.map(urls, fn url -> Client.fetch(url) end)
+    Enum.map(urls, fn url ->
+      {:ok, start_time} = DateTime.now("Etc/UTC")
+
+      Client.fetch(url)
+      |> append_elapsed_time(start_time)
+      |> print_response
+    end)
   end
+
+  defp append_elapsed_time(response, start_time) do
+     {:ok, end_time} = DateTime.now("Etc/UTC")
+      elapsed_time = DateTime.diff(start_time, end_time, :millisecond) * -1
+
+      response <> " #{elapsed_time}ms"
+  end
+
+  defp print_response(response), do: IO.puts(response)
 end
--- lib/parallel_download/client.ex
+++ lib/parallel_download/client.ex
@@ -1,7 +1,33 @@
 defmodule ParallelDownload.Client do
-  use Tesla, only: [:get], docs: false
+  use Tesla
+
+  @timeout 1_000
+
+  defp client() do
+    Tesla.client([
+      {Tesla.Middleware.Timeout, timeout: @timeout},
+    ])
+  end
 
   def fetch(url) do
-    get(url)
+    client()
+    |> get(url)
+    |> parse_response(url)
+  end
+
+  defp parse_response({:ok, %{status: status}}, url) do
+    "GET #{url} -> status #{status} in"
+  end
+
+  defp parse_response({:error, %{status: status}}, url) do
+    "ERROR #{url} -> status #{status} in"
+  end
+
+  defp parse_response({:error, reason}, url) when is_atom(reason) do
+    "IGNORED #{url} with reason #{reason} in"
+  end
+
+  defp parse_response({:error, _}, url) do
+    "IGNORED #{url} in"
   end
 end
